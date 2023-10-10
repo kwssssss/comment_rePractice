e7c3cdc8acf79e02683ff7f8167683dbec0b34bc
Kang Woo Sung <znlv0420@daum.net>
2023-10-10 23:00:26 +0900

    BoardMapper, ServiceImpl 작성

---
+++ src/main/java/org/galapagos/mapper/BoardMapper.java
@@ -0,0 +1,25 @@
+package org.galapagos.mapper;
+
+import java.util.List;
+
+import org.galapagos.domain.BoardVO;
+import org.galapagos.domain.Criteria;
+
+public interface BoardMapper {
+
+	public List<BoardVO> getList();
+	
+	public List<BoardVO> getListWithPaging(Criteria cri);
+	
+	public void insert(BoardVO board);
+	
+	public void insertSelectKey(BoardVO board);
+	
+	public BoardVO read(Long bno);
+	
+	public int delete(Long bno);
+	
+	public int update(BoardVO board);
+	
+	public int getTotalCount(Criteria cri);
+}
